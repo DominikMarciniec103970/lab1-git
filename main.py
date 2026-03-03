68c69cba9a0a8172501aa2f7a2708191d0faff89
Dominik Marciniec <s103970@pollub.edu.pl>
2026-03-03 23:16:39 +0100

    Pierwszy commit - dodanie main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+  Main.py - Super swietny program, ktory mnozy wektory.
+'''
+  
+import numpy as np
+  
+if __name__ == '__main__':
+      a = np.ones(5)*2
+      b = np.ones(5)*5
+  
+      print(a * b)
