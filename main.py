6c5805a8444d37d1e72c0a1990444653edf880d4
Dominik Marciniec <s103970@pollub.edu.pl>
2026-03-03 23:19:04 +0100

    Refaktoryzacja - dodanie funkcji arytmetycznych

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-'''
-  Main.py - Super swietny program, ktory mnozy wektory.
-'''
-  
 import numpy as np
   
-if __name__ == '__main__':
-      a = np.ones(5)*2
-      b = np.ones(5)*5
+def add(a, b):
+      return a + b
   
-      print(a * b)
+def subtract(a, b):
+      return a - b
+  
+def multiply(a, b):
+      return a * b
+  
+def divide(a, b):
+      return a / b
+  
+if __name__ == "__main__":
+      print(add(1, 2))
+      print(subtract(1, 2))
+      print(multiply(1, 2))
+      print(divide(1, 2))
